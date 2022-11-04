a88844df530c8e0155635c24fe7cac70b6cba399
David Calavera <dcalaver@amazon.com>
2022-11-04 20:42:46 +0000

    change cache directory
    Signed-off-by: David Calavera <dcalaver@amazon.com>

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const { cwd, chdir } = require('node:process');
 const { exec } = require('node:child_process');
+const { rename } = require('node:fs');
 
-process.env.DENO_DIR = `${cwd()}/cache`;
+rename(`${cwd()}/cache`, '/tmp/cache', (error) => {
+  if (error) throw error;
+});
+process.env.DENO_DIR = '/tmp/cache';
+
 console.log('cache dir:', process.env.DENO_DIR);
 
 chdir('app');
 const command = '../bin/deno run --allow-all main.ts';
-const { stdout, stderr } = exec(command, { port: 3000 }, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
+const { stdout, stderr } = exec(command, { port: 3000 }, (error) => {
+  if (error) throw error;
 });
 
 stdout.on('data', (data) => {
