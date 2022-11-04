983dcfe9f0d3e021a2c2031fed7a833fbb299cc8
David Calavera <dcalaver@amazon.com>
2022-11-04 22:39:52 +0000

    Bundle all together
    Signed-off-by: David Calavera <dcalaver@amazon.com>

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ build:
 	export DENO_INSTALL=${DENO_INSTAL:-$DEFAULT_DENO_INSTALL}
 	export DENO_DEPLOYMENT_ID=${AWS_COMMIT_ID:-1}
 	deno cache app/main.ts --import-map=app/import_map.json
+	cd app && deno bundle main.ts deno-server.js
 	mkdir -p .next/standalone
-	cp -r server.js ${DENO_INSTALL}/bin app cache .next/standalone/
+	cp -r server.js ${DENO_INSTALL}/bin app/deno-server.js static .next/standalone/
 
--- server.js
+++ server.js
@@ -2,15 +2,7 @@ const { cwd, chdir } = require('node:process');
 const { exec } = require('node:child_process');
 const { cp } = require('node:fs');
 
-cp(`${cwd()}/cache`, '/tmp/cache', { recursive: true }, (error) => {
-  if (error) throw error;
-});
-process.env.DENO_DIR = '/tmp/cache';
-
-console.log('cache dir:', process.env.DENO_DIR);
-
-chdir('app');
-const command = '../bin/deno run --allow-all main.ts';
+const command = 'bin/deno run --allow-all deno-server.js';
 const { stdout, stderr } = exec(command, { port: 3000 }, (error) => {
   if (error) throw error;
 });
