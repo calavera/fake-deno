32ff0ac80e753d4d39510d4f8e1304aae5184b8d
David Calavera <dcalaver@amazon.com>
2022-11-04 19:47:46 +0000

    log output and error
    Signed-off-by: David Calavera <dcalaver@amazon.com>

--- server.js
+++ server.js
@@ -3,4 +3,18 @@ const { exec } = require('node:child_process');
 process.env.DENO_DIR = `${process.env.PWD}/cache`;
 
 process.chdir('app');
-exec('deno run --allow-net --allow-env --allow-read main.ts', { port: 3000 });
+const command = 'deno run --allow-net --allow-env --allow-read main.ts';
+const { stdout, stderr } = exec(command, { port: 3000 }, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+});
+
+stdout.on('data', (data) => {
+  console.log(data.toString());
+});
+
+stderr.on('data', (data) => {
+  console.error(data.toString());
+});
