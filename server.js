7f445100754dd30aec59f6ba1cfd4fe5a22daeeb
David Calavera <dcalaver@amazon.com>
2022-11-04 23:13:14 +0000

    Set cache home
    Signed-off-by: David Calavera <dcalaver@amazon.com>

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { exec } = require('node:child_process');
 const { cp } = require('node:fs');
 
 process.env.DENO_DIR = '/tmp/cache';
+process.env.XDG_CACHE_HOME = '/tmp';
 
 const command = 'bin/deno run --allow-all deno-server.js';
 const { stdout, stderr } = exec(command, { port: 3000 }, (error) => {
