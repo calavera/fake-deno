5b56c3084281fb10502dbda7fbd76c301fd9fed4
David Calavera <dcalaver@amazon.com>
2022-11-04 20:08:30 +0000

    fix change directory
    Signed-off-by: David Calavera <dcalaver@amazon.com>

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const process = require('process');
+const { cwd, chdir } = require('node:process');
 const { exec } = require('node:child_process');
-process.env.DENO_DIR = `${process.env.PWD}/cache`;
 
-process.chdir('app');
+process.env.DENO_DIR = `${cwd()}/cache`;
+console.log('cache dir:', process.env.DENO_DIR);
+
+chdir('app');
 const command = '../bin/deno run --allow-net --allow-env --allow-read main.ts';
 const { stdout, stderr } = exec(command, { port: 3000 }, (error, stdout, stderr) => {
   if (error) {
